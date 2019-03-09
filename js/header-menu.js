971d1da25531dd7bbba772add40213ca1f8cc7bd
Sarah Lettmann <sarah@td-lettmann.de>
2019-03-09 14:44:27 +0100

    author for post, main menu now opening whilst pushing content down, post

--- js/header-menu.js
+++ js/header-menu.js
@@ -1,21 +1,28 @@
-headerContainer = $(".header .container");
-originalMarginHeader = headerContainer.outerHeight(true);
-
 function menuLinkClicked() {
 
     // set classes to show or hide the main menu and pushes main are down
+    var mainMenuToggle = $(".nav-link-menu");
     var mainMenu = $(".menu-main");
-    var menuHeight = $(".menu-main").height();
+    var menuHeight = parseInt($(".menu-main").height());
+    header = $(".header");
+    originalMarginHeader = 96;
 
     if(mainMenu.hasClass("menu-main--hidden")) {
-        mainMenu.removeClass("menu-main--hidden");
-        mainMenu.addClass("menu-main--visible");
-        // add height of menu to original margin of header
-        headerContainer.css("margin-bottom", originalMarginHeader + menuHeight);
+      // menu is hidden -> make it visible
+      mainMenu.removeClass("menu-main--hidden");
+      mainMenu.addClass("menu-main--visible");
+      // set aria-expanded=true
+      mainMenuToggle.attr("aria-expanded", true);
+      // add height of menu to original margin of header
+      newMargin =  originalMarginHeader + menuHeight;
+      header.css("margin-bottom", newMargin);
     } else {
-        mainMenu.removeClass("menu-main--visible");
-        mainMenu.addClass("menu-main--hidden");
-        // reset margin of header
-        headerContainer.css("margin-bottom", originalMarginHeader);
+      // manu is visible -> make it hidden
+      mainMenu.removeClass("menu-main--visible");
+      mainMenu.addClass("menu-main--hidden");
+      // set aria-expanded=false
+      mainMenuToggle.attr("aria-expanded", false);
+      // reset margin of header
+      header.css("margin-bottom", originalMarginHeader);
     }
 };
