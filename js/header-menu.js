a44ca23d80754e092448b10c5234e2b015adc675
Sarah Lettmann <sarah@td-lettmann.de>
2020-01-03 00:16:52 +0100

    refactoring, main menu

--- js/header-menu.js
+++ js/header-menu.js
@@ -1,95 +1,41 @@
 var maxWidth = 1236;
+var mainMenu = $(".main-menu");
 
-// set aria for desktop menu and accessability
-$(document).ready(function() {
-  // only do this for desktop menu, when hover is possible
-  if ($(window).width() >= maxWidth) {
-    $(".menu__item--has-submenu").hover(function() {
-      $(this).children(".menu__link").attr("aria-expanded","true");
-      $(this).children(".menu__link").prop("aria-expanded","true");
-      $(this).children(".menu__submenu").removeClass("menu--hidden");
-    }, function() {
-      $(this).children(".menu__link").attr("aria-expanded","false");
-      $(this).children(".menu__link").prop("aria-expanded","false");
-      $(this).children(".menu__submenu").addClass("menu--hidden");
-    });
-  }
-});
 
 // function to expand mobile menu when button is clicked
 $(".navigation__link").on("click", function() {
+  console.log("works");
   var mainMenuToggle = $(".navigation__link");
-  console.log(mainMenuToggle);
-  var mainMenu = $(".main-menu");
+  var burger = mainMenuToggle.children(".burger");
   var submenuToggle = $(".menu__item--has-submenu > .menu__link");
-  var submmenu = $(".menu__item--has-submenu > .menu__submenu");
-
+  // not the first time expanding main menu
   if(mainMenu.hasClass("menu--hidden")) {
     mainMenu.removeClass("menu--hidden");
     mainMenuToggle.attr("aria-expanded", true);
-    mainMenuToggle.prop("aria-expanded", true);
-    mainMenu.slideDown(200);
+    burger.addClass("burger--to-cross");
   } else {
     mainMenu.addClass("menu--hidden");
     mainMenuToggle.attr("aria-expanded", false);
-    mainMenuToggle.prop("aria-expanded", false);
-
-    // collapse submenus when opened
-    if(submmenu.hasClass("menu--visible")) {
-      submmenu.removeClass("menu--visible");
-      submmenu.addClass("menu--hidden");
-      // set aria-expanded=false
-      submenuToggle.attr("aria-expanded", false);
-      submenuToggle.prop("aria-expanded", false);
-      // slide up main menu and all submenus
-      submmenu.slideUp(200);
-    }
-    mainMenu.slideUp(200);
-
+    burger.removeClass("burger--to-cross");
   }
-
 });
 
-
-
-
 // function to expand mobile menu when button is clicked
-$(".menu__link--has-submenu").on("click", submenuOpenClose);
-function submenuOpenClose(e) {
-
-  // prevent default
-  e = e || window.event;
-  e.stopPropagation();
-  e.preventDefault();
-
-  if ($(window).width() < 1200) {
+$(".menu__link--has-submenu").on("click", function(e) {
+  if ($(window).width() < maxWidth) {
+    e = e || window.event;
+    e.stopPropagation();
+    e.preventDefault();
     // set classes to show or hide the main menu and pushes main area down
-    var submenuToggle = $(".menu__item--has-submenu > .menu__link");
-    var submmenu = $(".menu__item--has-submenu > .menu__submenu");
-    var mainMenu = $(".menu");
+    var submenuListItem = $(this).parent(".menu__item--has-submenu");
+    var submmenu = submenuListItem.children(".menu__submenu");
     var submenuHeight = parseInt(submmenu.height());
-
-    if(submmenu.hasClass("menu--hidden")) {
-
-      // submenu is hidden -> make it visible
-      submmenu.removeClass("menu--hidden");
-      submmenu.addClass("menu--visible");
-      // set aria-expanded=true
-      submenuToggle.attr("aria-expanded", true);
-      submenuToggle.prop("aria-expanded", true);
-      // slide submenu down
-      submmenu.slideDown(200);
-
+    if(submmenu.hasClass("menu__submenu--hidden")) {
+      submmenu.removeClass("menu__submenu--hidden");
+      $(this).attr("aria-expanded", true);
     } else {
-
-      // submenu is visible -> make it hidden
-      submmenu.removeClass("menu--visible");
-      submmenu.addClass("menu--hidden");
-      // set aria-expanded=false
-      submenuToggle.attr("aria-expanded", false);
-      submenuToggle.prop("aria-expanded", false);
-      // slide submenu up
-      submmenu.slideUp(200);
+      submmenu.addClass("menu__submenu--hidden");
+      $(this).attr("aria-expanded", false);
     }
   }
-};
+});
