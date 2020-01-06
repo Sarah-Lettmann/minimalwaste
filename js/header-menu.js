ee2e50fa2a6a89c5c4b6cea4165bf6c505416bb6
Sarah Lettmann <sarah@td-lettmann.de>
2020-01-06 16:40:20 +0100

    menu as yml data, refactoring header html

--- js/header-menu.js
+++ js/header-menu.js
@@ -2,8 +2,8 @@ var maxWidth = 1236;
 var mainMenu = $(".main-menu");
 
 // function to expand mobile menu when button is clicked
-$(".navigation__link").on("click", function() {
-  var mainMenuToggle = $(".navigation__link");
+$(".navigation__link--burger").on("click", function() {
+  var mainMenuToggle = $(this);
   var burger = mainMenuToggle.children(".burger");
   var submenuToggle = $(".menu__item--has-submenu > .menu__link");
   if(mainMenu.hasClass("menu--hidden")) {
@@ -19,22 +19,20 @@ $(".navigation__link").on("click", function() {
   }
 });
 
-// function to expand mobile menu when button is clicked
+// function to expand submenu when parent is clicked
 $(".menu__link--has-submenu").on("click", function(e) {
-  if ($(window).width() < maxWidth) {
-    e = e || window.event;
-    e.stopPropagation();
-    e.preventDefault();
-    // set classes to show or hide the main menu and pushes main area down
-    var submenuListItem = $(this).parent(".menu__item--has-submenu");
-    var submmenu = submenuListItem.children(".menu__submenu");
-    var submenuHeight = parseInt(submmenu.height());
-    if(submmenu.hasClass("menu__submenu--hidden")) {
-      submmenu.removeClass("menu__submenu--hidden");
-      $(this).attr("aria-expanded", true);
-    } else {
-      submmenu.addClass("menu__submenu--hidden");
-      $(this).attr("aria-expanded", false);
-    }
+  e = e || window.event;
+  e.stopPropagation();
+  e.preventDefault();
+  // set classes to show or hide the main menu and pushes main area down
+  var submenuListItem = $(this).parent(".menu__item--has-submenu");
+  var submmenu = submenuListItem.children(".menu__submenu");
+  var submenuHeight = parseInt(submmenu.height());
+  if(submmenu.hasClass("menu__submenu--hidden")) {
+    submmenu.removeClass("menu__submenu--hidden");
+    $(this).attr("aria-expanded", true);
+  } else {
+    submmenu.addClass("menu__submenu--hidden");
+    $(this).attr("aria-expanded", false);
   }
 });
