0338feb514e67fa0e2a07cba689c681ae3b79169
Sarah Lettmann <sarah@td-lettmann.de>
2020-03-07 03:03:21 +0100

    adsense removed, disqus without consent included to make comments useful

--- js/include-scripts.js
+++ js/include-scripts.js
@@ -5,13 +5,6 @@ window.addEventListener("load", function() {
 function includeScripts() {
   if(checkCookiesAllowed()) {
     console.log("cookies allowed, include scripts");
-    googleAdsense1 = document.createElement("script");
-    googleAdsense1.setAttribute("type", "text/javascript");
-    googleAdsense1.setAttribute("async", "");
-    googleAdsense1.setAttribute("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js");
-    googleAdsense2 = document.createElement("script");
-    googleAdsense2.setAttribute("type", "text/javascript");
-    googleAdsense2.setAttribute("src", "/js/google-adsense.js");
     googleTagManager1 = document.createElement("script");
     googleTagManager1.setAttribute("type", "text/javascript");
     googleTagManager1.setAttribute("src", "/js/google-tagmanager.js");
@@ -21,14 +14,8 @@ function includeScripts() {
     googleTagManager2.setAttribute("style", "display:none;visibility:hidden");
     googleTagManagerNoScript = document.createElement("noscript");
     googleTagManagerNoScript.appendChild(googleTagManager2);
-    disqus = document.createElement("script");
-    disqus.setAttribute("type", "text/javascript");
-    disqus.setAttribute("src", "/js/disqus.js");
-    document.getElementsByTagName("head")[0].appendChild(googleAdsense1);
-    document.getElementsByTagName("head")[0].appendChild(googleAdsense2);
     document.getElementsByTagName("head")[0].appendChild(googleTagManager1);
     var body = document.getElementsByTagName("body")[0];
     body.insertBefore(googleTagManagerNoScript, body.childNodes[0]);
-    document.querySelector(".footer-scripts").appendChild(disqus);
   }
 }
